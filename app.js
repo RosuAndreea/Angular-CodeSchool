cd18b5f6ce1f967a50fb4fbe682a652a31d8827c
Andreea Alupoaei <andreea.alupoaei@assist.ro>
2018-02-28 11:06:23 +0200

    Refactoring Product Gallery

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 (function (){
 	var app = angular.module("gemStore",[]);
 
-	app.controller('GalleryController',function(){
-		this.current = 0;
-		this.setCurrent = function(newGallery){
-			this.current = newGallery || 0;
-		}
-	});
-
 	app.controller('StoreController', function(){
 		this.products = gems;
 	});
@@ -51,11 +44,26 @@
 			templateUrl: "product-specs.html"
 		};
 	});
+
 	app.directive('productReviews', function(){
 		return {
 			restrict: "E",
 			templateUrl: "product-reviews.html"
 		};
+	});
+
+	app.directive('productGallery',function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'product-gallery.html',
+			controller: function(){
+				this.current = 0;
+				this.setCurrent = function(imageNumber){
+					this.current = imageNumber || 0;
+				};
+			},
+			controllerAs: 'gallery'
+		}
 	});
 
 	var gems = [{
